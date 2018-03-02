125d12a95034a1689e9f084093fd7518b16ab5f6
huangk <huangk@sjq-it.com>
2018-03-02 11:43:01 +0800

    rabbitmq example

---
+++ hk-amqp-example/src/main/java/com/hk/amqp/example/RabbitMQApplication.java
@@ -0,0 +1,17 @@
+package com.hk.amqp.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author kally
+ * @date 2018年3月2日上午11:18:14
+ */
+@SpringBootApplication
+public class RabbitMQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMQApplication.class, args);
+	}
+}
