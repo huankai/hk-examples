0477c19848e3273c42c687448e0526990d59fd5d
huangk <huankai@139.com>
2018-11-17 13:02:23 +0800

    删除

--- hk-kafka-example/hk-kafka-spring-cloud-stream-consumer-transaction/src/main/java/com/hk/springcloud/stream/kafka/transaction/TransactionApplication.java
+++
@@ -1,8 +0,0 @@
-package com.hk.springcloud.stream.kafka.transaction;
-
-/**
- * @author: sjq-278
- * @date: 2018-11-09 10:45
- */
-public class TransactionApplication {
-}
--- hk-kafka-example/hk-kafka-spring-cloud-stream-consumer-transaction/src/main/java/com/hk/springcloud/stream/kafka/transaction/repository/jdbc/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hk.springcloud.stream.kafka.transaction.repository.jdbc;
-
-/**
- * @author: sjq-278
- * @date: 2018-11-09 13:51
- */
-public interface CityRepository {
-}
