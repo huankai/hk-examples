3d392151b490eaa960f7a496528f740b7fff73f7
huangk <huankai@139.com>
2019-02-27 17:55:50 +0800

    缓存测试

--- hk-mysql-example/src/test/java/com/hk/mysql/example/cache/test/CacheTest.java
+++ hk-mysql-example/src/test/java/com/hk/mysql/example/cache/test/CacheTest.java
@@ -1,6 +1,6 @@
 package com.hk.mysql.example.cache.test;
 
-import com.hk.commons.util.JsonUtils;
+import com.hk.core.cache.NullCacheProperties;
 import com.hk.core.test.BaseTest;
 import com.hk.mysql.examples.MysqlExampleApplication;
 import com.hk.mysql.examples.domain.Account;
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 import java.util.concurrent.CyclicBarrier;
 
 /**
@@ -21,12 +22,46 @@ public class CacheTest extends BaseTest {
     @Autowired
     private AccountService accountService;
 
+    /**
+     * 缓存穿透测试：
+     * <pre>
+     *  id 为 1 的记录在数据中存在， id 为 5  的记录在数据库中不存在，
+     *  将数据库查询为 null 的记录(id 为 5)也保存在缓存中，但设置的缓存过期时间较短，防止在高并发下用户访问不存在的记录造成数据库压力过大，
+     *  为null 的缓存过期时间配置参数，可查看 {@link NullCacheProperties#getNullValueTtl()}
+     *
+     * </pre>
+     */
     @Test
-    public void test() throws InterruptedException {
-        int num = 10;
+    public void test2() throws InterruptedException {
+        int num = 100;
         CyclicBarrier cyclicBarrier = new CyclicBarrier(num);
         for (int i = 0; i < num; i++) {
-            new TestCyclicBarrier(cyclicBarrier).start();
+            new TestCyclicBarrier(cyclicBarrier, "1").start();
+        }
+
+//        num = 100;
+//        cyclicBarrier = new CyclicBarrier(num);
+//        for (int i = 0; i < num; i++) {
+//            new TestCyclicBarrier(cyclicBarrier, "5").start();
+//        }
+        Thread.currentThread().join();
+    }
+
+    /**
+     * 缓存雪崩测试:
+     * <pre>
+     *     在高并发条件下，多个线程同时查询同一记录，如果缓存中不存在 ，只需要有一个线程去数据库查询记录，放在缓存中，其它线程不需要从数据库中查询，直接在缓存中取数据，
+     *     实现方法，使用 {@link com.hk.core.cache.interceptor.LockCacheInterceptor} 加上 synchronized
+     * </pre>
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void test() throws InterruptedException {
+        int num = 1000;
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(num);
+        for (int i = 0; i < num; i++) {
+            new TestCyclicBarrier(cyclicBarrier, "1").start();
         }
         Thread.currentThread().join();
 
@@ -34,18 +69,21 @@ public class CacheTest extends BaseTest {
 
     private class TestCyclicBarrier extends Thread {
 
+        private String id;
+
         private CyclicBarrier cyclicBarrier;
 
-        private TestCyclicBarrier(CyclicBarrier cyclicBarrier) {
+        private TestCyclicBarrier(CyclicBarrier cyclicBarrier, String id) {
             this.cyclicBarrier = cyclicBarrier;
+            this.id = id;
         }
 
         @Override
         public void run() {
             try {
                 cyclicBarrier.await();
-                Account account = accountService.getById("1");
-                System.out.println(JsonUtils.serialize(account));
+                Optional<Account> account = accountService.findById(id);
+                System.out.println(account.isPresent());
             } catch (Exception e) {
                 e.printStackTrace();
             }
---
+++ hk-mysql-example/src/test/java/com/hk/mysql/example/test/SemesterTest.java
@@ -0,0 +1,53 @@
+package com.hk.mysql.example.test;
+
+import com.hk.commons.util.ArrayUtils;
+import com.hk.commons.util.JsonUtils;
+import com.hk.commons.util.date.DateTimeUtils;
+import com.hk.core.data.commons.query.Operator;
+import com.hk.core.data.jdbc.JdbcSession;
+import com.hk.core.data.jdbc.SelectArguments;
+import com.hk.core.data.jdbc.query.SimpleCondition;
+import com.hk.core.test.BaseTest;
+import com.hk.mysql.examples.MysqlExampleApplication;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
+/**
+ * @author huangkai
+ * @date 2019-02-27 16:18
+ */
+@SpringBootTest(classes = {MysqlExampleApplication.class})
+public class SemesterTest extends BaseTest {
+
+    @Autowired
+    private JdbcSession jdbcSession;
+
+    @Test
+    public void test() {
+        Set<String> schoolNames = ArrayUtils.asHashSet("执信中学琶洲实验学校（小学部）", "绿翠现代实验学校（小学部）", "海珠实验中学附属小学", "六中长风中学", "执信中学琶洲实验学校");
+        SelectArguments arguments = new SelectArguments();
+        arguments.setFrom("scm_school");
+        arguments.getConditions().addCondition(new SimpleCondition("sch_name", Operator.IN, schoolNames));
+        List<Map<String, Object>> result = jdbcSession.queryForList(arguments, false).getResult();
+        List<Object[]> schoolYearList = new ArrayList<>();
+        List<Object[]> semesterList = new ArrayList<>();
+        for (Map<String, Object> item : result) {
+            String schoolId = (String) item.get("schoolId");
+            String id = UUID.randomUUID().toString();
+            schoolYearList.add(new Object[]{id, schoolId, "2018~2019学年", DateTimeUtils.stringToDate("2018-09-01"), DateTimeUtils.stringToDate("2019-07-01"), "1", new Date(), new Date()});
+
+            semesterList.add(new Object[]{UUID.randomUUID().toString(), schoolId, "2018-~2019下学期", DateTimeUtils.stringToDate("2019-02-01"),
+                    DateTimeUtils.stringToDate("2019-07-01"), 20, 20, id, "1", "SEM2019", new Date(), new Date()});
+        }
+        System.out.println(JsonUtils.serialize(schoolYearList, true));
+        System.out.println(schoolYearList.size());
+        System.out.println(JsonUtils.serialize(semesterList, true));
+        jdbcSession.batchUpdate("INSERT INTO `scm_schoolyear`(`schoolyear_id`, `school_id`, `schoolyear_name`, `begin_date`, `end_date`, `due_status`, `last_up_time`, `create_time`) VALUES " +
+                "(?,?,?,?,?,?,?,?)", schoolYearList);
+        jdbcSession.batchUpdate("INSERT INTO `scm_semester`(`semester_id`, `school_id`, `semester_name`, `begin_date`, `end_date`, `total_week_no`, `class_week_no`, `schoolyear_id`, `due_status`, `semester_code`, `last_up_time`, `create_time`) VALUES " +
+                "(?,?,?,?,?,?,?,?,?,?,?,?)", semesterList);
+    }
+}
