71e57073392d0e79f729736285fb32b0ac848e18
huangkai <huankai@139.com>
2019-03-26 22:57:53 +0800

    security 示例

--- hk-security-example/src/main/java/com/hk/security/example/config/SecurityExampleConfiguration.java
+++ hk-security-example/src/main/java/com/hk/security/example/config/SecurityExampleConfiguration.java
@@ -1,8 +1,11 @@
 package com.hk.security.example.config;
 
+import com.hk.commons.JsonResult;
+import com.hk.commons.util.JsonUtils;
 import com.hk.core.authentication.security.expression.AdminAccessWebSecurityExpressionHandler;
 import com.hk.core.authentication.security.handler.logout.RedirectLogoutHandler;
 import com.hk.core.autoconfigure.authentication.security.AuthenticationProperties;
+import com.hk.core.web.Webs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +15,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.authentication.session.CompositeSessionAuthenticationStrategy;
+import org.springframework.security.web.session.InvalidSessionStrategy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author huangkai
@@ -68,12 +81,30 @@ public class SecurityExampleConfiguration extends WebSecurityConfigurerAdapter {
                 .usernameParameter(browser.getUsernameParameter())
                 .passwordParameter(browser.getPasswordParameter())
                 .loginProcessingUrl(browser.getLoginProcessingUrl())
-
+                .and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint(browser.getLoginUrl()) {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                if (Webs.isAndroid(request) || Webs.isIPhone(request)) {
+                    Webs.writeJson(response, 200, JsonResult.unauthorized("用户未认证"));
+                } else {
+                    super.commence(request, response, authException);
+                }
+            }
+        })
                 .and()
 //                .rememberMe().disable()//禁用remember-me功能
                 .sessionManagement()
+                .sessionAuthenticationStrategy(new CompositeSessionAuthenticationStrategy(new ArrayList<>()))
                 .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .enableSessionUrlRewriting(false)
+                .invalidSessionStrategy((request, response) -> {
+                    if (Webs.isAndroid(request) || Webs.isIPhone(request)) {
+                        Webs.writeJson(response, 200, JsonResult.unauthorized("用户未认证"));
+                    } else {
+                        request.getSession();
+                        response.sendRedirect(browser.getLoginUrl());
+                    }
+                })
                 .maximumSessions(browser.getMaximumSessions())
                 .sessionRegistry(sessionRegistry())
                 .maxSessionsPreventsLogin(browser.isMaxSessionsPreventsLogin())
