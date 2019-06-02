d3721a6e20c5f135bb127e873f4c41bebb6fd277
huangkai <huankai@139.com>
2019-06-02 22:53:30 +0800

    mycat 测试

---
+++ hk-mycat-example/src/main/java/com/hk/mycat/example/entity/User.java
@@ -0,0 +1,39 @@
+package com.hk.mycat.example.entity;
+
+import com.hk.core.data.jpa.domain.AbstractUUIDPersistable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author huangkai
+ * @date 2019-06-02 21:48
+ */
+
+@Data
+@Entity
+@Table(name = "user")
+@EqualsAndHashCode(callSuper = true)
+public class User extends AbstractUUIDPersistable {
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "pass_word")
+    private String password;
+
+    @Column(name = "brithday")
+    private LocalDate brithday;
+
+    @Column(name = "sex")
+    private Integer sex;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+}
---
+++ hk-mycat-example/src/main/java/com/hk/mycat/example/repository/jpa/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hk.mycat.example.repository.jpa;
+
+import com.hk.core.data.jpa.repository.StringIdJpaRepository;
+import com.hk.mycat.example.entity.User;
+
+/**
+ * @author huangkai
+ * @date 2019-06-02 21:50
+ */
+public interface UserRepository extends StringIdJpaRepository<User> {
+
+}
